bf23f1dba75168b91eae07f8b94e93de1de93404
fernal73 <linus.fernandes@gmail.com>
2021-03-26 12:51:22 +0530

    Add numeric.php

---
+++ numeric.php
@@ -0,0 +1,32 @@
+<?php
+// round number up
+// output: 19
+$num = 19.7;
+echo floor($num);
+echo nl2br("\n");
+// round number down
+// output: 20
+echo ceil($num);
+echo nl2br("\n");
+// return absolute value of number
+// output: 19.7
+$num = -19.7;
+echo abs($num);
+echo nl2br("\n");
+// calculate 4 ^ 3
+// output: 64
+echo pow(4,3);
+echo nl2br("\n");
+// calculate natural log of 100
+// output: 2.30258509299
+echo log(10);
+echo nl2br("\n");
+// calculate log of 100, base 10
+// output: 2
+echo log(100,10);
+echo nl2br("\n");
+// calculate exponent of 2.30258509299
+// output: 9.99999999996
+echo exp(2.30258509299);
+echo nl2br("\n");
+?>
