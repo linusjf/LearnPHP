8cbe8ac8461bf59f70828c768efe886b8eeca6a5
fernal73 <linus.fernandes@gmail.com>
2021-03-26 12:58:33 +0530

    Add more methods to numeric.php

--- numeric.php
+++ numeric.php
@@ -29,4 +29,35 @@ echo nl2br("\n");
 // output: 9.99999999996
 echo exp(2.30258509299);
 echo nl2br("\n");
+
+echo rand();
+echo nl2br("\n");
+
+// generate a random number between 10 and 99
+echo rand(10,99);
+echo nl2br("\n");
+// convert to binary
+// output: 1000
+echo decbin(8);
+echo nl2br("\n");
+// convert to hexadecimal
+// output: 8
+echo dechex(8);
+echo nl2br("\n");
+// convert to octal
+// output: 10
+echo decoct(8);
+echo nl2br("\n");
+// convert from octal
+// output: 8
+echo octdec(10);
+echo nl2br("\n");
+// convert from hexadecimal
+// output: 101
+echo hexdec(65);
+echo nl2br("\n");
+// convert from binary
+// output: 8
+echo bindec(1000);
+echo nl2br("\n");
 ?>
